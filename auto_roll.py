d233b4eeca68b988ed4f1e16bf73a790a321bce6
Revolves <49674023+Revolves@users.noreply.github.com>
2022-02-23 14:59:49 +0800

    update

---
+++ auto_roll.py
@@ -0,0 +1,10 @@
+from time import time
+import win32api
+import win32con
+import time
+import os
+# -1代表向下移动一个单位，-100也会向下移动一个单位，都是一个单位哦，亲~
+os.path.isf
+while True:
+  time.sleep(1)
+  win32api.mouse_event(win32con.MOUSEEVENTF_WHEEL,0,0,-100)
